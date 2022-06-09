a50749cb5adb4bad51c1ae729308032d6d121a3e
superpotato <khaled.battiche@esprit.tn>
2022-06-09 15:20:21 +0100

    burrowing done

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-
+use near_sdk::serde::Deserialize;
 use near_sdk::{
-    env, ext_contract, log, near_bindgen, AccountId, Gas, PanicOnDefault, Promise, PromiseResult,
+    env, ext_contract, log, near_bindgen, serde, AccountId, Gas, PanicOnDefault, Promise,
+    PromiseResult,
 };
-use std::convert::{TryFrom, TryInto};
+
 use std::str::{self, FromStr};
 pub const TGAS: u64 = 1_000_000_000_000;
 pub const NO_DEPOSIT: u128 = 0;
@@ -17,21 +18,26 @@ pub struct Contract {
     pub wrapper_contract: AccountId,
 }
 
-// cross call contract to get metadata from potato token
 #[ext_contract(ext_ft)]
 pub trait PotatoToken {
     #[payable]
-
     fn ft_metadata(&mut self) -> String;
     fn query_greeting_callback(&mut self) -> String;
     fn ft_transfer_call(&mut self, amount: String, msg: String, receiver_id: String);
     fn make_deposit_callback(&mut self) -> bool;
+    fn oracle_call(&mut self, receiver_id: String, msg: String);
+}
+
+#[ext_contract(ext_collateral)]
+#[derive(Deserialize)]
+#[serde(crate = "near_sdk::serde")]
+pub trait Collateral {
+    fn execute(&mut self, actions: Vec<String>);
 }
 
 #[near_bindgen]
 impl Contract {
     #[init]
-
     pub fn new(burrow_contract: AccountId, wrapper_contract: AccountId) -> Self {
         assert!(!env::state_exists(), "Already initialized");
         Self {
@@ -66,23 +72,34 @@ impl Contract {
         return promise;
     }
 
-    #[private] // Public - but only callable by env::current_account_id()
-    pub fn make_deposit_callback(&self) -> bool {
-        let deposit_burrow: String = match env::promise_result(0) {
-            PromiseResult::Successful(value) => str::from_utf8(&value).unwrap().to_string(),
-            _ => {
-                log!("There was an error contacting Hello NEAR");
-                return false;
-            }
-        };
+    #[payable]
+    pub fn increase_colateral(&mut self, actions: Vec<String>) -> Promise {
+        assert!(
+            env::prepaid_gas() >= Gas::from(20 * TGAS),
+            "Please attach at least 20 TGAS"
+        );
 
-        return deposit_burrow.to_string() == "true".to_string();
+        let account = "contract.1638481328.burrow.testnet";
+        let promise = ext_collateral::execute(
+            actions.to_vec(),
+            AccountId::from_str(account).unwrap(),
+            1,
+            Gas(50000000000000),
+        );
+
+        return promise;
+    }
+
+    pub fn make_burrow(&mut self, msg: String, receiver_id: AccountId) -> Promise {
+        let account = "priceoracle.testnet";
+        let promise = ext_ft::oracle_call(
+            receiver_id.to_string(),
+            msg,
+            AccountId::from_str(account).unwrap(),
+            1,
+            Gas(150000000000000),
+        );
+        return promise;
     }
 }
 
-// amount,
-// msg,
-// receiver_id.to_string(),
-// self.wrapper_contract.clone(),
-// YOCTO_NEAR,
-// Gas::from(30 * TGAS),
